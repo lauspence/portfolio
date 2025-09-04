4b42a0a4412e2b78da452077669c835830421cbc
Lawrence Muriithi <laurencemmarete@gmail.com>
2025-09-04 13:36:42 +0300

    Fix settings and wsgi paths for Render

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -36,7 +36,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'portfolio.urls'
+ROOT_URLCONF = 'portfolio.portfolio.urls'
 
 TEMPLATES = [
     {
@@ -53,7 +53,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'portfolio.wsgi.application'
+WSGI_APPLICATION = 'portfolio.portfolio.wsgi.application'
 
 # Database (SQLite for now; you can switch to Postgres on Render if needed)
 DATABASES = {
