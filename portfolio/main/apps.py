0ee2f5a0cdf1beeaf54d07922198a5efd8d3a9aa
Lawrence Muriithi <laurencemmarete@gmail.com>
2025-09-12 00:29:28 +0300

    Add auto superuser creation in apps.py

--- portfolio/main/apps.py
+++ portfolio/main/apps.py
@@ -1,6 +1,17 @@
+# main/apps.py
 from django.apps import AppConfig
-
+from django.conf import settings
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'portfolio.main'
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+        if not User.objects.filter(username="lau").exists():
+            User.objects.create_superuser(
+                username="lau",
+                email="laurencemmarete@gmail.com",
+                password="Pass123"
+            )
