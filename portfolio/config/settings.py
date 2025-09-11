30d24cf556de35303181acfa80b748928a647a4c
Lawrence Muriithi <laurencemmarete@gmail.com>
2025-09-11 18:02:36 +0300

    Fix STATICFILES_DIRS path

--- portfolio/config/settings.py
+++ portfolio/config/settings.py
@@ -79,7 +79,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'portfolio', 'main', 'static')]
+STATICFILES_DIRS = [BASE_DIR /  'main' / 'static']
 STATIC_ROOT = BASE_DIR / "staticfiles"  # for Render deployment
 # WhiteNoise storage backend
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
