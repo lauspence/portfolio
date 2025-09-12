735d18873b4ee4516376ef3e836093189fa80bc9
Lawrence Muriithi <laurencemmarete@gmail.com>
2025-09-12 17:38:51 +0300

    Auto-create superuser on first deploy

--- portfolio/main/apps.py
+++ portfolio/main/apps.py
@@ -4,3 +4,14 @@ from django.apps import AppConfig
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'portfolio.main'
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+        if not User.objects.filter(username='lau').exists():
+            User.objects.create_superuser(
+                username='lau',
+                email='maretelau@gmail.com',
+                password='Pass123'
+            )
+            print("Superuser 'lau' created with password 'Pass123'")
