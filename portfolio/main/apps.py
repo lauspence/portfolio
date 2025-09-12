3bcace19db769b182d3ced8b6826b7697a044490
Lawrence Muriithi <laurencemmarete@gmail.com>
2025-09-12 17:47:45 +0300

    Fix apps.py to create superuser safely after migrations

--- portfolio/main/apps.py
+++ portfolio/main/apps.py
@@ -1,17 +1,17 @@
+# main/apps.py
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
+from django.contrib.auth import get_user_model
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'portfolio.main'
 
     def ready(self):
-        from django.contrib.auth import get_user_model
-        User = get_user_model()
-        if not User.objects.filter(username='lau').exists():
-            User.objects.create_superuser(
-                username='lau',
-                email='maretelau@gmail.com',
-                password='Pass123'
-            )
-            print("Superuser 'lau' created with password 'Pass123'")
+        post_migrate.connect(create_default_user, sender=self)
+
+def create_default_user(sender, **kwargs):
+    User = get_user_model()
+    if not User.objects.filter(username='lau').exists():
+        User.objects.create_superuser('lau', 'maretelau@gmail.com', 'Pass123')
+
